1389c27b5ccf69b186a325cc31220dd5c35025b3
Ahmed Muhsin <ahmedmuhsin@microsoft.com>
2025-04-11 15:18:34 -0500

    finalize hello world

--- HelloWorld/src/main/java/com/function/Function.java
+++ HelloWorld/src/main/java/com/function/Function.java
@@ -12,20 +12,20 @@ import com.microsoft.azure.functions.annotation.McpToolTrigger;
 public class Function {
     public final String SNIPPET_NAME_PROPERTY_NAME = "snippetName";
     public final String SNIPPET_PROPERTY_NAME = "snippet";
-    public final String SAVE_SNIPPET_ARGUMENT = 
-    "[" +
-    "   {" +
-    "      \"propertyName\":\"snippetName\"," +
-    "      \"propertyType\":\"string\"," +
-    "      \"description\":\"The name of the snippet.\"" +
-    "   }," +
-    "   {" +
-    "      \"propertyName\":\"snippet\"," +
-    "      \"propertyType\":\"string\"," +
-    "      \"description\":\"The content of the snippet.\"" +
-    "   }" +
-    "]";
-
+    public final String SAVE_SNIPPET_ARGUMENT = """
+        [
+           {
+              "propertyName":"snippetName",
+              "propertyType":"string",
+              "description":"The name of the snippet."
+           },
+           {
+              "propertyName":"snippet",
+              "propertyType":"string",
+              "description":"The content of the snippet."
+           }
+        ]
+    """;
 
     /**
     * This function is triggered by an MCP Tool Trigger.
@@ -35,7 +35,7 @@ public class Function {
     * @param context The execution context of the function, used for logging.
     */
     @FunctionName("HelloWorld")
-    public void logCustomTriggerInput(
+    public String logCustomTriggerInput(
     @McpToolTrigger(
         toolName = "getsnippets",
         description = "Gets code snippets from your snippet collection.",
@@ -44,5 +44,7 @@ public class Function {
         final ExecutionContext context) {
             context.getLogger().info(triggerInput);
             context.getLogger().info("Hello, World!");
+
+            return "I am MCP";
         }
 }
